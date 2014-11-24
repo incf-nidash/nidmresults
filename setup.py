b4fb62217c22cb7d79190e127bf69c18d61068a7
cmaumet <c.m.j.maumet@warwick.ac.uk>
2014-11-24 12:21:54 +0000

    First-pass at creating a separate package for generic nidm-results exporter s

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+readme = open('README.md').read()
+
+requirements = [
+    'prov',
+    'nibabel',
+    'numpy'    
+]
+
+setup(
+    name = "nidmresults",
+    version = "0.1.0",
+    author = "Camille Maumet",
+    author_email = "c.m.j.maumet@warwick.ac.uk",
+    description = ("Export of neuroimaging statistical results using NIDM"
+        " as specified at http://nidm.nidash.org/specs/nidm-results.html."),
+    license = "BSD",
+    keywords = "Prov, NIDM, Provenance",
+    packages=find_packages(),
+    package_dir={
+        'nidmresults': 'nidmresults'
+    },
+    long_description=readme,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
