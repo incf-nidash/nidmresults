a514020e0f07f8cbe9852d1f7037c33f3a8713d3
cmaumet <c.m.j.maumet@warwick.ac.uk>
2016-09-06 14:22:46 +0100

    Update version to 1.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = list(filter(None, reqs))
 
 setup(
     name="nidmresults",
-    version="0.3.4",
+    version="1.0.0",
     author="Camille Maumet",
     author_email="c.m.j.maumet@warwick.ac.uk",
     description=(
