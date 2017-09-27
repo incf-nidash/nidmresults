258c6d4472a5a364bfb94084276bbd482f30deda
cmaumet <cmaumet@users.noreply.github.com>
2017-09-27 09:03:51 +0100

    fix: more nidmres export ids missing in GT

--- test/test_reader.py
+++ test/test_reader.py
@@ -105,6 +105,18 @@ class TestReader(unittest.TestCase, TestResultDataModel):
                 export_act_id = 'f219fd38405d063a1ed632fcb2147443'
             elif 'ex_spm_temporal_derivative' in nidmpack:
                 export_act_id = '659c46e2d1c780606dffaab2340049d2'
+            elif 'ex_spm_thr_clustfwep05' in nidmpack:
+                export_act_id = '67aaee6777a38b8d12724eac8d77eb3e'
+            elif 'ex_spm_thr_clustunck10' in nidmpack:
+                export_act_id = 'd917e70463d0a73d9dd1b9ebd8edbf12'
+            elif 'ex_spm_thr_voxelfdrp05' in nidmpack:
+                export_act_id ='824058887a650fffd1658a75fffd3204'
+            elif 'ex_spm_thr_voxelfwep05' in nidmpack:
+                export_act_id = '5dacfe9b4b68e90ce4283bf549bb2b50'
+            elif 'ex_spm_thr_voxelunct4' in nidmpack:
+                export_act_id = '3629454df7cfe3fbfb4c3ebc41023e67'
+            else:
+                export_act_id = ''
 
 
             # This is a workaround to avoid confusion between attribute and class uncorrected p-value
