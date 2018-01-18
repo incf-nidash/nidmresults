c26b9bb8b67a287fd39f24fbbfac14fa10d8a864
cmaumet <camille.maumet@inria.fr>
2018-01-18 15:15:11 +0100

    test: Uncomment download data from NeuroVault if not available

--- test/test_reader.py
+++ test/test_reader.py
@@ -10,8 +10,8 @@ import unittest
 from nidmresults.graph import *
 from nidmresults.test.test_results_doc import TestResultDataModel
 from future.standard_library import hooks
-# with hooks():
-#     from urllib.request import urlopen, Request
+with hooks():
+    from urllib.request import urlopen, Request
 
 from nidmresults.owl.owl_reader import OwlReader
 
@@ -45,30 +45,30 @@ class TestReader(unittest.TestCase, TestResultDataModel):
         if not os.path.exists(data_dir):
             os.makedirs(data_dir)
 
-        # # Collection containing examples of NIDM-Results packs (1.3.0)
-        # req = Request(
-        #     "http://neurovault.org/api/collections/2210/nidm_results")
-        # rep = urlopen(req)
+        # Collection containing examples of NIDM-Results packs (1.3.0)
+        req = Request(
+            "http://neurovault.org/api/collections/2210/nidm_results")
+        rep = urlopen(req)
 
-        # response = rep.read()
-        # data = json.loads(response.decode('utf-8'))
+        response = rep.read()
+        data = json.loads(response.decode('utf-8'))
 
-        # # Download the NIDM-Results packs from NeuroVault if not available
-        # # locally
-        # self.packs = list()
-        # for nidm_res in data["results"]:
-        #     url = nidm_res["zip_file"]
-        #     study = nidm_res["name"]
+        # Download the NIDM-Results packs from NeuroVault if not available
+        # locally
+        self.packs = list()
+        for nidm_res in data["results"]:
+            url = nidm_res["zip_file"]
+            study = nidm_res["name"]
 
-        #     nidmpack = os.path.join(data_dir, study + ".zip")
-        #     if not os.path.isfile(nidmpack):
-        #         f = urlopen(url)
-        #         print("downloading " + url + " at " + nidmpack)
-        #         with open(nidmpack, "wb") as local_file:
-        #             local_file.write(f.read())
-        #     self.packs.append(nidmpack)
+            nidmpack = os.path.join(data_dir, study + ".zip")
+            if not os.path.isfile(nidmpack):
+                f = urlopen(url)
+                print("downloading " + url + " at " + nidmpack)
+                with open(nidmpack, "wb") as local_file:
+                    local_file.write(f.read())
+            self.packs.append(nidmpack)
 
-        self.packs = glob.glob(os.path.join(data_dir, '*.nidm.zip'))
+        # self.packs = glob.glob(os.path.join(data_dir, '*.nidm.zip'))
         self.out_dir = os.path.join(data_dir, 'recomputed')
 
         if os.path.isdir(self.out_dir):
