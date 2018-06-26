4360c6aa28eabc7f36ce759d32abd772ebc2f2f6
cmaumet <camille.maumet@inria.fr>
2018-06-26 10:55:19 +0200

    Exception if loading unhandled file

--- nidmresults/load.py
+++ nidmresults/load.py
@@ -24,5 +24,7 @@ def load(filename, to_replace=dict()):
         raise Exception('Minimal json file: not handled yet')
     elif filename.endswith('.nidm.zip'):
         nidm = NIDMResults.load_from_pack(filename, to_replace=to_replace)
+    else:
+        raise Exception('Unhandled format ' + filename)
 
     return nidm
