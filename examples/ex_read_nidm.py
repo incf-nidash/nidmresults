8dc5786a527aae9b94e5b99f85d0d7a928a9a570
cmaumet <camille.maumet@inria.fr>
2018-06-26 11:01:27 +0200

    fixing PEP8

--- examples/ex_read_nidm.py
+++ examples/ex_read_nidm.py
@@ -11,8 +11,8 @@ nidm_url = 'https://neurovault.org/collections/2210/fsl_default_130.nidm.zip'
 nidmpack = "2210_fsl_default_130.nidm.zip"
 
 if not os.path.isfile(nidmpack):
-	print('Downloading ' + nidmpack)
-	urllib.request.urlretrieve(nidm_url, nidmpack)
+    print('Downloading ' + nidmpack)
+    urllib.request.urlretrieve(nidm_url, nidmpack)
  
 # Known issues with NIDM packs in collection 2210
 to_replace = {
@@ -24,4 +24,3 @@ to_replace = {
 nres = nidm.load(nidmpack, to_replace=to_replace)
 
 print(json.dumps(nres.get_info(), indent=4))
- 
