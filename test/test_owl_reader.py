f6b2f2df1c946d4192900b4c24f4f56cc4bda31e
Remi Gau <remi_gau@hotmail.com>
2024-02-20 19:52:28 +0100

    add doc string

--- test/test_owl_reader.py
+++ test/test_owl_reader.py
@@ -2,6 +2,6 @@ from nidmresults.owl.owl_reader import OwlReader
 
 
 def test_OwlReader(owl_file):
-
+    """Smoke test of several methods."""
     owl = OwlReader(owl_file=owl_file)
     owl.count_by_namespaces()
