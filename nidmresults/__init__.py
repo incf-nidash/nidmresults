bd42798bd8ae897ba1bb9b38e735afe6d0ddcbb0
Remi Gau <remi_gau@hotmail.com>
2024-02-20 07:51:58 +0100

    rely on hacthling vcs for versioning

--- nidmresults/__init__.py
+++ nidmresults/__init__.py
@@ -1,10 +1,7 @@
 import os
-from pkg_resources import get_distribution
 
-from .load import load
+from nidmresults._version import __version__
 
 latest_owlfile = os.path.join(
     os.path.dirname(os.path.dirname(__file__)), 'nidmresults', 'owl',
     "nidm-results_130.owl")
-
-__version__ = get_distribution('nidmresults').version
