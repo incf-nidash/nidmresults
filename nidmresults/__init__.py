595ff826fd3d767ed37dca923f5b17e625008caa
Remi Gau <remi_gau@hotmail.com>
2024-02-20 08:25:51 +0100

    add load to init

--- nidmresults/__init__.py
+++ nidmresults/__init__.py
@@ -1,7 +1,11 @@
 import os
 
+from nidmresults.load import load
+
 from nidmresults._version import __version__
 
 latest_owlfile = os.path.join(
     os.path.dirname(os.path.dirname(__file__)), 'nidmresults', 'owl',
     "nidm-results_130.owl")
+
+__all__ = ["load"]
